259a117ef59a1c2c2f60108bbba0ef43967c5cbc
lukito1234567 <166683653+lukito1234567@users.noreply.github.com>
2024-04-18 16:26:54 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,58 +1,28 @@
-const season1 =(spring) => {
-    if(spring > 10 && spring < 15){
-        return "Spring temperature: " + spring;
-    }
-}
-const season2 =(summer) => {
-    if(summer > 30 && summer < 40){
-        return "Summer temperature: " + summer;
-    }
-}
-const season3 =(autumn) => {
-    if(autumn > 5 && autumn < 15){
-        return "Autumn temperature: " + autumn;
-    }
-}
-const season4 =(winter) => {
-    if(winter > -5 && winter < 10){
-        return "Winter temperature: " + winter;
-    }
-}
-const callSeason=(season,temperature) =>{
-    switch(season){
-        case "spring":
-        return season1(temperature);
-        break;
-        case "summer":
-        return season2(temperature);
-        break;
-        case "autumn":
-        return season3(temperature);
-        break;
-        case "winter":
-        return season4(temperature);
-        break;
-        default:
-        return "Invalid season";
-    }   
-}
-console.log(callSeason("spring",20));
-console.log(callSeason("summer",30));
-console.log(callSeason("autumn",10));
-console.log(callSeason("winter",9));
+let carbrand = ["ford", "Audi", "BMW", "Toyota", "Honda", "ferrari"];
+let carArray = car.length;
+let num = document.querySelector(".num");
+num.innerHTML = carArray;
 
+let calc = 0;
+let button1 = document.querySelector(".btn1");
+let button2 = document.querySelector(".btn2");
+let button3 = document.querySelector(".btn3");
 
-for (let i = -5; i < 40; i++){
-    if(i > 10 && i < 24){
-        console.log("Spring",i);
-    }else if(i > 20 && i < 40){
-        console.log("Summer",i);
-    }else if (i > 5 && i < 16){
-        console.log("Autumn",i);
-    }else if(i > -5 && i < 10){
-        console.log("winter",i);
-    }else{
-        console.log("invalid season");
+button1.addEventListener("click", () => {
+    if (calc >= 0 && calc < 10) {
+        calc++;
+        num.innerHTML = calc;
     }
-}
- 
+});
+
+button2.addEventListener("click", () => {
+    calc = 0;
+    num.innerHTML = calc;
+});
+
+button3.addEventListener("click", () => {
+    if (calc > 0 && calc <= 10) {
+        calc--;
+        num.innerHTML = calc;
+    }
+});
