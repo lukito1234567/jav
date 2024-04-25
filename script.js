67dd87bc662f89b6f1472be646984ac0cf4ae38e
lukito1234567 <166683653+lukito1234567@users.noreply.github.com>
2024-04-25 09:42:32 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,37 @@
-let Numbers = [1, 7, 5, 3, 2, 6, 10, 16, 26, 22, 8, 9];
-
+let  myNumbers = [1, 7, 5, 3, 2, 6, 10, 16, 26, 22, 8, 9];
 let sum = 0;
-
-Numbers.map((num) => {
-    sum = sum + num;
-});
-
+myNumbers.forEach((item)=>{
+    sum +=item;
+})
 console.log(sum);
 
-const filt = Numbers.filter (num => num % 2 === 0 );
-.
-console.log(filt);
 
-let numArray = [7, 12, 23, 15, 5];
-
-console.log(numArray);
-
-let wordArray = ['orange' , 'cherry', 'pineapple' , 'apple'];
-
-let ulElement = document.createElement('ul');
+const filetered = myNumbers.filter(num => num > 2);
+console.log(filetered);
+ 
 
 
-wordArray.forEach ((word) =>{
-    let liElement = document.createElement('li');
-    liElement.textContent = word;
-    ulElement.appendChild(liElement);
+let newArray = [7, 12, "orange", "cherry", 23, 15, "pineapple", "apple", 5];
+let stringArray = [];
+let numArray = [];
+
+newArray.forEach((item) => {
+if (typeof item === "string") {
+stringArray.push(item);
+} else {
+numArray.push(item);
+}
 });
 
-console.log(ulElement);
+console.log(stringArray); 
+console.log(numArray); 
 
 
 
-
-
-
-
-
-
-
-
+let ulElement = document.createElement("ul");
+stringArray.forEach((item)=>{
+    let liElement = document.createElement("li");
+    liElement.innerHTML = item;
+    ulElement.appendChild(liElement);
+});
+document.body.appendChild(ulElement);
