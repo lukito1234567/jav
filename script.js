7833f03a74fd300d48fa7c4217703129936aab5e
lukito1234567 <166683653+lukito1234567@users.noreply.github.com>
2024-04-28 13:07:26 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,37 +1,17 @@
-let  myNumbers = [1, 7, 5, 3, 2, 6, 10, 16, 26, 22, 8, 9];
-let sum = 0;
-myNumbers.forEach((item)=>{
-    sum +=item;
+let box = document.querySelectorAll(".car");
+box.forEach((grid)=>{
+    grid.addEventListener("click",()=>{
+        let parent = grid.parentElement;
+        parent.remove();
+    })
 })
-console.log(sum);
-
-
-const filetered = myNumbers.filter(num => num > 2);
-console.log(filetered);
- 
-
-
-let newArray = [7, 12, "orange", "cherry", 23, 15, "pineapple", "apple", 5];
-let stringArray = [];
-let numArray = [];
-
-newArray.forEach((item) => {
-if (typeof item === "string") {
-stringArray.push(item);
-} else {
-numArray.push(item);
-}
-});
-
-console.log(stringArray); 
-console.log(numArray); 
 
 
 
-let ulElement = document.createElement("ul");
-stringArray.forEach((item)=>{
-    let liElement = document.createElement("li");
-    liElement.innerHTML = item;
-    ulElement.appendChild(liElement);
-});
-document.body.appendChild(ulElement);
+
+
+
+
+
+
+
