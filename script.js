ace1ae3d4c1f3fd5c4ee975db053f1fefce5be4b
lukito1234567 <166683653+lukito1234567@users.noreply.github.com>
2024-05-01 10:28:00 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,48 @@
-let box = document.querySelectorAll(".car");
-box.forEach((grid)=>{
-    grid.addEventListener("click",()=>{
-        let parent = grid.parentElement;
-        parent.remove();
-    })
-})
+
+function createList() {
+
+    let ul = document.createElement("ul");
+
+   
+    let items = ["Item 1", "Item 2", "Item 3"];
+
+   
+    items.forEach(function(itemText) {
+        let li = document.createElement("li");
+        li.textContent = itemText; 
+        ul.appendChild(li); 
+    });
+
+  
+    let container = document.getElementById("listContainer");
+
+ 
+    container.appendChild(ul);
+}
+
+document.addEventListener("DOMContentLoaded", createList);
 
 
+let createdElement; 
 
 
+function createAndAppendElement() {
+  
+    createdElement = document.createElement("p");
+    createdElement.textContent = "This is item.";
 
 
+    document.getElementById("container").appendChild(createdElement);
+}
+
+let remover = () =>{
+    deleteButton.forEach(button =>{
+        button.addEventListener('click' , () =>{
+            let die = button.parentElement;
+            die.remove();
+        });
+    });
+}
 
 
 
