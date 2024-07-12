6abb2caf25015dc1a62fcc8d21bbfadd43c77218
lukito1234567 <166683653+lukito1234567@users.noreply.github.com>
2024-07-12 21:05:45 +0400

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const NotesContainer = styled.div`
+  background-color: #1e1e1e;
+  color: white;
+  padding: 2rem;
+  text-align: center;
+`;
+
+const Notes = () => {
+  return (
+    <NotesContainer>
+      <h2>Notes</h2>
+      
+    </NotesContainer>
+  );
+};
+
+export default Notes;
